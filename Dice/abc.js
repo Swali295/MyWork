27260a5e626e73587d6f29d0eb585ae5a04f9310
SWALI SAXENA <56640671+Swali295@users.noreply.github.com>
2020-10-02 12:16:09 +0530

    Add files via upload

---
+++ Dice/abc.js
@@ -0,0 +1,6 @@
+var superheros= require('superheroes');
+mysuperhero=superheros.random();
+console.log(mysuperhero);
+
+var supervillains = require('supervillains');
+console.log(supervillains.random());
